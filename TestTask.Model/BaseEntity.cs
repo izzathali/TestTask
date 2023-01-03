35d4b7f24adac923ccc847a89d61cc6816570c71
Izzath Ali <izzath.info@gmail.com>
2023-01-03 14:40:28 +0530

    Add project files.

---
+++ TestTask.Model/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Model
+{
+    public class BaseEntity
+    {
+        public bool IsDeleted { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+    }
+}
